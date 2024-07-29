620b1bfc2bcb04d86598e4d46735ff296fd8a2ba
Senth <70545656+rr-senthamizh-samikkanu@users.noreply.github.com>
2024-07-28 17:37:25 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,11 @@ data_path = 'test_data.json'
 
 def load_test_data(path):
     with open(path) as data_file:
-        data = json.load(data_file)
+        data = json.load(data_file)  # returns the data from json file (list/dict)
         return data
 
+# any test that calls this fixture, will call the test for every parameter in the params;
+# so you an have multiple test cases to call this params-- it's kinda global; so the param data is in one place, and not in every test case;
 @fixture(params=[webdriver.Chrome, webdriver.Firefox, webdriver.Edge])
 def browser(request):
     driver = request.param
